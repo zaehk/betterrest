f49ed24f4a62ecaee81e2c54d35c7cbd7e99e6da
Borja Sáez de Guinoa Vilaplana <borja.saezdeguinoa@gmail.com>
2021-06-27 18:45:46 +0200

    initial commit

---
+++ betterrest/betterrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterrestApp.swift
+//  betterrest
+//
+//  Created by Borja Saez de Guinoa Vilaplana on 27/6/21.
+//
+
+import SwiftUI
+
+@main
+struct betterrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
